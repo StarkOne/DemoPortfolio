d85a91779452f23030887e13ebce08d23f48ee5e
StarkOne <Stark.pro40@gmail.com>
2017-03-17 09:56:32 +0200

    section menu

--- app/js/common.js
+++ app/js/common.js
@@ -4,5 +4,9 @@ $(function() {
 		$(this).toggleClass("active");
 		$(".left_side").toggleClass("active");
 	});
-
+	$(window).on("load", function() {
+		$(".loader_inner").fadeOut();
+		$(".loader").delay(400).fadeOut("slow");
+	});
 });
+
